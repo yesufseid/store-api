c163d4511d4c7382cd0eca0f6d575f8b9891bb89
seid yesuf <seidyesuf750@gmail.com>
2023-03-29 11:15:15 -0700

    day two

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,35 @@
+const product=require("../models/product")
+
+
 
 const getallproductstatic=async(req,res)=>{
-    res.status(200).json({msg:"tsting peoduct statisc"})
+    const search=req.query
+    console.log(search);
+    const products=await product.find({}).sort(search)
+    res.status(200).json({msg:products})
 }
 const getallproduct=async(req,res)=>{
-    throw Error("accsess dinaed")
-    res.status(200).json({msg:"tsting peoduct"})
+    const {feacherd,company,name,sort}=req.query
+    const objectquery={}
+    if(feacherd){
+        objectquery.feacherd=feacherd==="true" ? true:false
+    }
+    if(company){
+        objectquery.company=company
+    }
+    if(name){
+        objectquery.name={$regex:name,$options:"i"}
+    } 
+    console.log(sort);
+    let result= product.find(objectquery)
+    if(sort){
+        console.log(sort);
+     const result=result.sort()
+    }
+    const products=await result
+    res.status(200).json({products,nbHits:products.length})
+    
+    
 }
 
 
